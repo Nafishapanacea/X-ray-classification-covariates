fca919e035d3148777fc6ab56f55b9143bec5dee
Nafishapanacea <nafisha.begum@radpretation.ai>
2026-01-20 19:16:48 +0530

    CLAHE implemented

--- main/utils/.ipynb_checkpoints/transforms-checkpoint.py
+++ main/utils/.ipynb_checkpoints/transforms-checkpoint.py
@@ -6,12 +6,16 @@ def get_train_transform():
         transforms.RandomHorizontalFlip(),
         transforms.RandomRotation(10),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485]*3, std=[0.229]*3)
+        # transforms.Normalize(mean=[0.485]*3, std=[0.229]*3)
+        transforms.Normalize(mean=[0.502]*3, std=[0.284]*3)
     ])
 
 def get_val_transform():
     return transforms.Compose([
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485]*3, std=[0.229]*3)
+        # transforms.Normalize(mean=[0.485]*3, std=[0.229]*3)
+        transforms.Normalize(mean=[0.502]*3, std=[0.284]*3)
     ])
+
+    # Mean: 0.502022, Std: 0.284344
